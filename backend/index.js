d20ae5d30db65e43ac4937e2ae63ebaf2577cda3
CongPhiNguyen <congphinguyen312@gmail.com>
2022-12-18 06:01:58 +0700

    basic without payment ok

--- backend/index.js
+++ backend/index.js
@@ -15,42 +15,42 @@ const io = require("socket.io")(http, {
 })
 
 let users = [
-  {
-    userIdSocket: "f6eukhQWY10eRjsjAAA5",
-    list: [
-      {
-        idProduct: 1,
-        amount: 25
-      }
-    ]
-  },
-  {
-    userIdSocket: "y6fLS3RaCVwbYq9dAAA9",
-    list: [
-      {
-        idProduct: 2,
-        amount: 23
-      }
-    ]
-  },
-  {
-    userIdSocket: "1Enm3ReEN3-ovuwqAAA_",
-    list: [
-      {
-        idProduct: 3,
-        amount: 11
-      }
-    ]
-  },
-  {
-    userIdSocket: "LwTvQL8UIrC_bLZAAAAv",
-    list: [
-      {
-        idProduct: 4,
-        amount: 21
-      }
-    ]
-  }
+  // {
+  //   userIdSocket: "f6eukhQWY10eRjsjAAA5",
+  //   list: [
+  //     {
+  //       idProduct: 1,
+  //       amount: 25
+  //     }
+  //   ]
+  // }
+  // {
+  //   userIdSocket: "y6fLS3RaCVwbYq9dAAA9",
+  //   list: [
+  //     {
+  //       idProduct: 2,
+  //       amount: 23
+  //     }
+  //   ]
+  // },
+  // {
+  //   userIdSocket: "1Enm3ReEN3-ovuwqAAA_",
+  //   list: [
+  //     {
+  //       idProduct: 3,
+  //       amount: 11
+  //     }
+  //   ]
+  // },
+  // {
+  //   userIdSocket: "LwTvQL8UIrC_bLZAAAAv",
+  //   list: [
+  //     {
+  //       idProduct: 4,
+  //       amount: 21
+  //     }
+  //   ]
+  // }
 ]
 
 let admins = []
@@ -58,10 +58,44 @@ let admins = []
 io.on("connection", (socket) => {
   socket.emit("me", socket.id)
   socket.emit("getListCart", { users })
+
   socket.on("admin-join", (socketId) => {
-    console.log("admin join with id " + socketId)
+    // console.log("admin join with id " + socketId)
     admins.push(socketId)
   })
+
+  socket.on("user-join", (socketId) => {
+    console.log("socketId", socketId)
+    users.push({ userIdSocket: socketId, list: [] })
+    console.log("users", users)
+    admins.forEach((admin) => {
+      socket.to(admin).emit("update-user", users)
+    })
+  })
+
+  socket.on("update-product-user", (value) => {
+    users = users.map((user) => {
+      if (user.userIdSocket == value.id) {
+        return {
+          userIdSocket: user.userIdSocket,
+          list: value.product
+        }
+      } else return user
+    })
+
+    admins.forEach((admin) => {
+      socket.to(admin).emit("getListCart", { users, idSocket: value.id })
+    })
+
+    admins.forEach((admin) => {
+      socket.to(admin).emit("update-user", users)
+    })
+
+    // users.push({ userIdSocket: socketId, list: [] })
+    // console.log("users", users)
+  })
+
+  // console.log("admins", admins)
 
   socket.on("update-cart", (data) => {
     users = users.map((user) => {
@@ -81,6 +115,7 @@ io.on("connection", (socket) => {
       }
       return user
     })
+
     admins.forEach((admin) => {
       socket.to(admin).emit("getListCart", { users, idSocket: data.idSocket })
     })
@@ -89,6 +124,14 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("someone disconnected " + socket.id)
     admins = admins.filter((id) => id !== socket.id)
+    console.log("users", users)
+    users = users.filter((user) => {
+      console.log("user", user)
+      return user?.userIdSocket !== socket.id
+    })
+    admins.forEach((admin) => {
+      socket.to(admin).emit("update-user", users)
+    })
   })
 })
 
--- frontend/src/page/Payment.js
+++ frontend/src/page/Payment.js
@@ -1,168 +1,229 @@
-import { Layout, Menu, Row, Col, Skeleton } from 'antd';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Cart from './Cart';
-import { AntDesignOutlined, UserOutlined } from '@ant-design/icons';
-import { Avatar, Divider, Tooltip } from 'antd';
-import QRCode from "react-qr-code";
-import { useEffect, useRef } from 'react';
-import socketIOClient from 'socket.io-client';
+import { Layout, Menu, Row, Col, Skeleton } from "antd"
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Cart from "./Cart"
+import { AntDesignOutlined, UserOutlined } from "@ant-design/icons"
+import { Avatar, Divider, Tooltip } from "antd"
+import QRCode from "react-qr-code"
+import { useEffect, useRef } from "react"
+import socketIOClient from "socket.io-client"
 import data from "./data.json"
 
-const { Header, Content, Footer } = Layout;
-const host = 'http://localhost:5050'
+const { Header, Content, Footer } = Layout
+const host = "http://localhost:5050"
 const Payment = () => {
-    const navigate = useNavigate()
-    const socketRef = useRef()
-    const [users, setUser] = useState([])
-    const [userSelect, setUserSelect] = useState(null)
-    const [totalMoney, setTotalMoney] = useState(null)
-    const [reRenderQr, setRenderQr] = useState(true)
-    useEffect(() => {
-        socketRef.current = socketIOClient.connect(host)
-        socketRef.current.on('me', id => {
-            socketRef.current.emit("admin-join", id)
-        })
-        socketRef.current.on('getListCart', (data) => {
-            setUserSelect(data.users.find(cart => cart.userIdSocket === data.idSocket))
-            setUser(data.users)
-        })
-        return () => {
-            socketRef.current.disconnect()
-        }
-    }, [])
-    useEffect(() => {
-        setRenderQr(true)
-        if (userSelect) {
-            let total = 0;
-            userSelect.list.forEach(item => {
-                const filter = data.find(filter => filter.id == item.idProduct)
-                total += filter.price * item.amount
-            })
-            setTotalMoney(total)
-        }
-    }, [userSelect])
-    useEffect(() => {
-        if (totalMoney) {
-            setRenderQr(false)
-        }
-    }, [totalMoney])
-
-    const updateCart = (idProduct, amount) => {
-        socketRef.current.emit("update-cart", { idSocket: userSelect.userIdSocket, idProduct, amount })
-        let newList = [...users]
-        newList = newList.map(user => {
-            if (user.userIdSocket === userSelect.userIdSocket) {
-                if (amount === 0) {
-                    user.list = user.list.filter(item => item.idProduct !== idProduct)
-                } else {
-                    user.list = user.list.map(item => {
-                        if (item.idProduct === idProduct) {
-                            item.amount = amount
-                        }
-                        return item
-                    })
-                }
-            }
-            return user
-        })
-        setUser([...newList])
-        const newUserSelect = newList.find(user => user.userIdSocket === userSelect.userIdSocket)
-        setUserSelect({ ...newUserSelect })
-        let total = 0;
-        newUserSelect.list.forEach(item => {
-            const filter = data.find(filter => filter.id == item.idProduct)
-            total += filter.price * item.amount
-        })
-        setTotalMoney(total)
+  const navigate = useNavigate()
+  const socketRef = useRef()
+  const [users, setUser] = useState([])
+  const [userSelect, setUserSelect] = useState(null)
+  const [totalMoney, setTotalMoney] = useState(null)
+  const [reRenderQr, setRenderQr] = useState(true)
+  useEffect(() => {
+    socketRef.current = socketIOClient.connect(host)
+    socketRef.current.on("me", (id) => {
+      socketRef.current.emit("admin-join", id)
+    })
+    socketRef.current.on("update-user", (data) => {
+      setUser(data)
+    })
+    socketRef.current.on("getListCart", (data) => {
+      console.log("data", data)
+      setUserSelect(
+        data.users.find((cart) => cart.userIdSocket === data.idSocket)
+      )
+      setUser(data.users)
+    })
+    return () => {
+      socketRef.current.disconnect()
     }
+  }, [])
+  useEffect(() => {
+    setRenderQr(true)
+    if (userSelect) {
+      let total = 0
+      userSelect.list.forEach((item) => {
+        const filter = data.find((filter) => filter.id == item.idProduct)
+        total += filter.price * item.amount
+      })
+      setTotalMoney(total)
+    }
+  }, [userSelect])
+  useEffect(() => {
+    if (totalMoney) {
+      setRenderQr(false)
+    }
+  }, [totalMoney])
 
-    return (
-        <Layout className="layout">
-            <Header>
-                <div className="logo" />
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['3']}
-                    items={
-                        [
-                            {
-                                key: 1,
-                                label: "Home",
-                                onClick: () => { navigate("/") }
-                            },
+  const updateCart = (idProduct, amount) => {
+    socketRef.current.emit("update-cart", {
+      idSocket: userSelect.userIdSocket,
+      idProduct,
+      amount
+    })
+    let newList = [...users]
+    newList = newList.map((user) => {
+      if (user.userIdSocket === userSelect.userIdSocket) {
+        if (amount === 0) {
+          user.list = user.list.filter((item) => item.idProduct !== idProduct)
+        } else {
+          user.list = user.list.map((item) => {
+            if (item.idProduct === idProduct) {
+              item.amount = amount
+            }
+            return item
+          })
+        }
+      }
+      return user
+    })
+    setUser([...newList])
+    const newUserSelect = newList.find(
+      (user) => user.userIdSocket === userSelect.userIdSocket
+    )
+    setUserSelect({ ...newUserSelect })
+    let total = 0
+    newUserSelect.list.forEach((item) => {
+      const filter = data.find((filter) => filter.id == item.idProduct)
+      total += filter.price * item.amount
+    })
+    setTotalMoney(total)
+  }
 
-                            {
-                                key: 3,
-                                label: "Payment",
-                                onClick: () => { navigate("/payment") }
-                            },
-                        ]
-                    }
-                />
-            </Header>
-            <Content
-                style={{
-                    padding: '0 50px',
-                }}
-            >
-                <div className="site-layout-content">
-                    <Row style={{ marginTop: "50px" }} gutter={[16, 16]}>
-                        <Col span={8}>
-                            {
-                                reRenderQr ? (<Skeleton active />) : (
-                                    <div style={{ background: "black", display: "flex", borderRadius: "8px", width: "100%", flexDirection: "column", alignItems: "center", padding: "5px 10px" }}>
-                                        <QRCode
-                                            size={256}
-                                            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                                            viewBox={`0 0 256 256`}
-                                            value={String(totalMoney)} />
-                                        <div style={{ textAlign: "center", color: "white", margin: "5px 0", fontWeight: "600", fontSize: "2rem" }}>SCAN ME</div>
-                                    </div>
-                                )
-                            }
-                        </Col>
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["3"]}
+          items={[
+            {
+              key: 1,
+              label: "Home",
+              onClick: () => {
+                navigate("/")
+              }
+            },
 
-                        <Col span={10} offset={3}>
-                            <div style={{ display: "flex", alignItems: "center" }}>
-                                <h1 style={{ marginBottom: "30px", paddingRight: "20px" }}>Users:</h1>
-                                <Avatar.Group maxCount={2} maxStyle={{ color: '#f56a00', backgroundColor: '#fde3cf', marginBottom: "50px" }}>
-                                    {
-                                        users.map(user => {
-                                            return (
-                                                <Tooltip onClick={() => { setUserSelect(user) }} title={user.userIdSocket} placement="top">
-                                                    <Avatar size={50} style={{ backgroundColor: '#f56a00', cursor: "pointer" }}>{user.userIdSocket}</Avatar>
-                                                </Tooltip>
-                                            )
-                                        })
-                                    }
-                                </Avatar.Group>
-                            </div>
-                            {
-                                userSelect && (
-                                    <>
-                                        <Cart updateCart={updateCart} items={userSelect.list}></Cart>
-                                        <div style={{ fontSize: "2rem", fontWeight: "700", color: "#95de64", textAlign: "center" }}>
-                                            TOTAL: <span>{totalMoney}</span>
-                                        </div>
-                                    </>
-                                )
-                            }
-
-                        </Col>
-                    </Row>
+            {
+              key: 3,
+              label: "Payment",
+              onClick: () => {
+                navigate("/payment")
+              }
+            }
+          ]}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 50px"
+        }}
+      >
+        <div className="site-layout-content">
+          <Row style={{ marginTop: "50px" }} gutter={[16, 16]}>
+            <Col span={8}>
+              {reRenderQr ? (
+                <Skeleton active />
+              ) : (
+                <div
+                  style={{
+                    background: "black",
+                    display: "flex",
+                    borderRadius: "8px",
+                    width: "100%",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    padding: "5px 10px"
+                  }}
+                >
+                  <QRCode
+                    size={256}
+                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    viewBox={`0 0 256 256`}
+                    value={String(totalMoney)}
+                  />
+                  <div
+                    style={{
+                      textAlign: "center",
+                      color: "white",
+                      margin: "5px 0",
+                      fontWeight: "600",
+                      fontSize: "2rem"
+                    }}
+                  >
+                    SCAN ME
+                  </div>
                 </div>
-            </Content>
-            <Footer
-                style={{
-                    textAlign: 'center',
-                }}
-            >
-                Ant Design ©2018 Created by Ant UED
-            </Footer>
-        </Layout>
-    );
+              )}
+            </Col>
+
+            <Col span={10} offset={3}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <h1 style={{ marginBottom: "30px", paddingRight: "20px" }}>
+                  Users:
+                </h1>
+                <Avatar.Group
+                  maxCount={2}
+                  maxStyle={{
+                    color: "#f56a00",
+                    backgroundColor: "#fde3cf",
+                    marginBottom: "50px"
+                  }}
+                >
+                  {users.map((user) => {
+                    return (
+                      <Tooltip
+                        onClick={() => {
+                          setUserSelect(user)
+                        }}
+                        title={user.userIdSocket}
+                        placement="top"
+                      >
+                        <Avatar
+                          size={50}
+                          style={{
+                            backgroundColor: "#f56a00",
+                            cursor: "pointer"
+                          }}
+                        >
+                          {user.userIdSocket}
+                        </Avatar>
+                      </Tooltip>
+                    )
+                  })}
+                </Avatar.Group>
+              </div>
+              {JSON.stringify(userSelect?.userIdSocket)}
+              {userSelect && (
+                <>
+                  <Cart updateCart={updateCart} items={userSelect.list}></Cart>
+                  <div
+                    style={{
+                      fontSize: "2rem",
+                      fontWeight: "700",
+                      color: "#95de64",
+                      textAlign: "center"
+                    }}
+                  >
+                    TOTAL: <span>{totalMoney}</span>
+                  </div>
+                </>
+              )}
+            </Col>
+          </Row>
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center"
+        }}
+      >
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  )
 }
 
-export default Payment;
+export default Payment
